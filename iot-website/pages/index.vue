200c7a05f96425fd952efe93db2dbe53e48dfaf9
Mario Arturo Sanchez Ruelas <marioarturosanchez@gmail.com>
2024-04-04 16:28:55 -0600

    Change button style

--- iot-website/pages/index.vue
+++ iot-website/pages/index.vue
@@ -3,15 +3,13 @@
         <v-container>
             <v-row>
                 <v-col>
-                    <v-btn to="/login" rounded="lg" height="150" width="70%"  class="mt-5 ml-7 px-16">
-                        <v-icon icon='mdi-clock-outline' size="x-large" class="mr-2"></v-icon>
+                    <v-btn to="/login" rounded="lg" prepend-icon="mdi-clock-outline" size="large" height="150" width="70%"  class="mt-5 ml-7 px-16" stacked>
                         <v-text class="text-center">Accesos Entrada</v-text>
                     </v-btn>
                     
                 </v-col>
                 <v-col>
-                    <v-btn to="/gym_access" rounded="lg" height="150" width="70%" class="mt-5 ml-7 px-16">
-                        <v-icon icon='mdi-weight-lifter' size="x-large" class="mr-2"></v-icon>
+                    <v-btn to="/gym_access" rounded="lg" prepend-icon="mdi-weight-lifter" size="large" height="150" width="70%" class="mt-5 ml-7 px-16" stacked>
                         <v-text class="text-center">Accesos Gimnasio</v-text>
                     </v-btn>
                     
@@ -20,15 +18,13 @@
             </v-row>
             <v-row>
                 <v-col>
-                    <v-btn to="/course_attendance" rounded="lg" height="150" width="70%"class="mt-5 ml-7 px-16">
-                        <v-icon icon='mdi-human-male-board' size="x-large" class="mr-2"></v-icon>
+                    <v-btn to="/course_attendance" rounded="lg" prepend-icon='mdi-human-male-board' size="large" height="150" width="70%"class="mt-5 ml-7 px-16" stacked>
                         <v-text class="text-center">Asistencias Cursos</v-text>
                     </v-btn>
                     
                 </v-col>
                 <v-col>
-                    <v-btn to="/login" rounded="lg" height="150" width="70%" class="mt-5 ml-7 px-16">
-                        <v-icon icon='mdi-book' size="x-large" class="mr-2"></v-icon>
+                    <v-btn to="/login" rounded="lg" prepend-icon='mdi-book' size="large" height="150" width="70%" class="mt-5 ml-7 px-16" stacked>
                         <v-text class="text-center">Historial Préstamo <br>de Material</v-text>
                             
                     </v-btn>
@@ -37,15 +33,13 @@
             </v-row>
             <v-row>
                 <v-col>
-                    <v-btn to="/login" rounded="lg" height="150" width="70%" class="mt-5 ml-7 px-16">
-                        <v-icon icon='mdi-car' size="x-large" class="mr-2"></v-icon>
+                    <v-btn to="/login" rounded="lg" prepend-icon='mdi-car' size="large" height="150" width="70%" class="mt-5 ml-7 px-16" stacked>
                         <v-text class="text-center">Historial Pagos <br>de <br>Estacionamiento</v-text>
                     </v-btn>
                     
                 </v-col>
                 <v-col>
-                    <v-btn to="/room_reservation" rounded="lg" height="150" width="70%" class="mt-5 ml-7 px-16">
-                        <v-icon icon='mdi-calendar-range' size="x-large" class="mr-2"></v-icon>
+                    <v-btn to="/room_reservation" rounded="lg" prepend-icon='mdi-calendar-range' size="large"height="150" width="70%" class="mt-5 ml-7 px-16" stacked>
                         <v-text class="text-center">Reservas de <br>Salones</v-text>
                     </v-btn>
                     
