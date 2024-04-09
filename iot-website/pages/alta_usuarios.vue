bf29cabb84e1a23d75696d432bab06e4d3d43c3b
Omar Atriano <omar.atrianova@udlap.com>
2024-04-09 17:05:48 -0600

    9Abril

--- iot-website/pages/alta_usuarios.vue
+++ iot-website/pages/alta_usuarios.vue
@@ -8,6 +8,21 @@
           label="Nombre"
           required
         ></v-text-field>
+
+        <v-text-field
+      v-model="user.email"
+      label="Correo Electrónico"
+      :rules="emailRules"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="user.password"
+      label="Contraseña"
+      type="password"
+      :rules="passwordRules"
+      required
+    ></v-text-field>
   
         <v-text-field
           v-model="user.id"
@@ -39,30 +54,42 @@
   
   <script>
   export default {
-    name: 'UserRegistration',
-    data() {
-      return {
-        valid: true,
-        user: {
-          name: '',
-          id: '',
-          role: '',
-          career: ''
-        },
-        roles: ['Estudiante', 'Profesor', 'Administrativo'],
-        nameRules: [
-          v => !!v || 'El nombre es requerido',
-          v => (v && v.length <= 10) || 'El nombre debe ser de 10 caracteres máximo'
-        ],
-        idRules: [
-          v => !!v || 'El ID es requerido',
-          v => !isNaN(parseFloat(v)) && isFinite(v) || 'El ID debe ser numérico'
-        ],
-        careerRules: [
-          v => !!v || 'La carrera es requerida'
-        ]
-      }
-    },
+  name: 'UserRegistration',
+  data() {
+    return {
+      valid: true,
+      user: {
+        name: '',
+        id: '',
+        role: '',
+        career: '',
+        email: '',    // Nuevo campo de correo electrónico
+        password: ''  // Nuevo campo de contraseña
+      },
+      roles: ['Estudiante', 'Profesor', 'Administrativo'],
+      nameRules: [
+        v => !!v || 'El nombre es requerido',
+        v => (v && v.length <= 10) || 'El nombre debe ser de 10 caracteres máximo'
+      ],
+      idRules: [
+        v => !!v || 'El ID es requerido',
+        v => !isNaN(parseFloat(v)) && isFinite(v) || 'El ID debe ser numérico'
+      ],
+      careerRules: [
+        v => !!v || 'La carrera es requerida'
+      ],
+      // Reglas de validación para el correo electrónico
+      emailRules: [
+        v => !!v || 'El correo electrónico es requerido',
+        v => /.+@.+\..+/.test(v) || 'El correo electrónico debe ser válido'
+      ],
+      // Reglas de validación para la contraseña
+      passwordRules: [
+        v => !!v || 'La contraseña es requerida',
+        v => v.length >= 6 || 'La contraseña debe tener al menos 6 caracteres'
+      ]
+    }
+  },
     methods: {
       submitForm() {
         if (this.$refs.form.validate()) {
